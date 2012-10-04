6b216846dbed838f538fe311004f014413e0633f
Eric Relson <gertlex@gmail.com>
2012-10-03 21:52:57 -0500

    Initial commit

---
+++ PyDyPackets.py
@@ -0,0 +1,155 @@
+import httplib
+import base64
+import StringIO
+import threading
+import time
+import wx
+import logging  # prevents different threads' output from mixing
+
+import serial
+
+if __name__ == '__main__':
+    
+    
+    logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-10s) %(message)s',)
+                        
+    class BytePacket(object):
+        def __init__(self, start=0):
+            self.lock = threading.Lock()
+            self.word = []
+        def increment(self):
+            # logging.debug('Waiting for lock; {0}'.format(self.value))
+            print self.lock.acquire()
+                
+            try:
+                # logging.debug('Acquired lock')
+                self.value = self.value + 1
+            finally:
+                self.lock.release()
+                
+                
+    # xBee serial port settings
+    myPort = 16 #15 #~ note: use port number - 1
+    # myPort = 16 #15 #~ note: use port number - 1
+    myBaud = 1000000
+    
+    def COMThread(byte_packet):
+        """
+        Receives:
+        byte_packet - a BytePacket
+        """
+        byte_string = ""
+        logging.debug( 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+        byte_list = list()
+        byte_packet.word = list()
+        while True:
+        
+            # If xbee serial port was opened and is open.
+            if serExist and ser.isOpen():
+                # read all bytes in the buffer
+                byte = None
+                while ser.inWaiting():
+                    
+                    # print '...'
+                    
+                    byte = ser.read()
+                    
+                    if byte != None and byte !="": 
+                        try:
+                            
+                            byte_list.append(int(byte.encode('hex'),16))
+                            # print int(byte.encode('hex'),16), byte_list[-2:]
+                        except ValueError as e:
+                            logging.debug(( 'error', byte, e,))
+                    
+                    if byte_list[-2:] == [0xff,0xff]:
+                        # print "packet:", [0xff,0xff], byte_list[:-2]
+                        # print "old:", byte_packet.word
+                        byte_packet.word = [0xff,0xff] + byte_list[:-2]
+                        logging.debug(("packet:", byte_packet.word,))
+                        byte_list = list()
+                        
+                    if len(byte_list) > 12:
+                        byte_list = list()
+                    # byte_string = byte_string + byte
+                    # byte_list.append(int(byte.encode('hex'),16))
+                    # if the end of packet symbol is seen...
+                    # if byte == ')':
+                    # if byte_string[:-2] == str(0xFF)+str(0xFF)):
+                    
+                # if byte != None and byte !="": 
+                    # try:
+                        # print int(byte.encode('hex'),16)
+                    # except ValueError as e:
+                        # print 'error', byte, e
+    
+    def OtherThreadMethod(byte_packet,outputfile="default_out"):
+        myoldbytelist = None
+        with open(outputfile,'w') as fw:
+            try:
+                while True:
+                    if byte_packet.word != myoldbytelist:
+                        myoldbytelist = byte_packet.word
+                        # logging.debug(("bahhaah", myoldbytelist,))
+                        # logging.debug("")
+                        fw.write(" ".join([str(x) for x in myoldbytelist]))
+                        fw.write("\n")
+                        
+                    # if byte_packet != []:
+                        # print "and ", byte_packet
+            except KeyboardInterrupt:
+                print "KeyboardInterrupt caught! Closing {0}...".format(outputfile)
+                
+        return
+    
+    # Opening the serial port
+    try:
+        ser = serial.Serial(myPort,myBaud) #port number - 1
+        print "Successfully connected to port {0} at {1} baud".format(myPort,myBaud)
+        serExist = 1
+    
+    except serial.SerialException:
+        print "Couldn't open xBee serial port. Try again later."
+        serExist = 0
+    
+    
+    # campanel = CameraPanel(frame, camera)
+    # campanel.SetSize((820,480))
+    
+    myoldbytelist = None
+    mybyte_list = BytePacket()
+    
+    thread = threading.Thread(target=COMThread, args=(mybyte_list,))
+    # thread = threading.Thread(target=COMThread, kwargs={'byte_packet': mybyte_list})
+    thread.daemon = True
+    thread.start()
+    
+    # thread2 = threading.Thread(target=OtherThreadMethod, args=(mybyte_list,))
+    # thread2.daemon = True
+    # thread2.start()
+    
+    # while True:
+        # # if mybyte_list.word != []:
+        # # if mybyte_list.word != myoldbytelist:
+            # # myoldbytelist = mybyte_list.word
+            # # print myoldbytelist
+            # # # print mybyte_list
+        # pass
+    outputfile="default_out"
+    with open(outputfile,'w') as fw:
+        try:
+            while True:
+                if mybyte_list.word != myoldbytelist:
+                    myoldbytelist = mybyte_list.word
+                    # logging.debug(("bahhaah", myoldbytelist,))
+                    # logging.debug("")
+                    fw.write(" ".join([str(x) for x in myoldbytelist]))
+                    fw.write("\n")
+                    
+        except KeyboardInterrupt:
+            print "KeyboardInterrupt caught! Closing {0}...".format(outputfile)
+    
+    # app.MainLoop()
+    
+    
